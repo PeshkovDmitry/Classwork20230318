2adb3b6208c73138e1b7b3ad9c68e8c72de7c1c8
Peshkov Dmitry <peshkov-dv@yandex.ru>
2023-03-18 14:49:22 +0300

    Task28

---
+++ Task28/Program.cs
@@ -0,0 +1,9 @@
+﻿int Factorial(int n) {
+    int f = 1;
+    for (int i = 1; i <= n; i++)
+        f *= i;
+    return f; 
+}
+
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Factorial(n));
